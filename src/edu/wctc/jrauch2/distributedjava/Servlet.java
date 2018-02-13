173ef79a1c0191d5ea4f226fbb55b7c31de47796
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-02-13 01:26:06 -0600

    fix Servlet

--- src/edu/wctc/jrauch2/distributedjava/Servlet.java
+++ src/edu/wctc/jrauch2/distributedjava/Servlet.java
@@ -1,16 +1,18 @@
 package edu.wctc.jrauch2.distributedjava;
 
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 
 @javax.servlet.annotation.WebServlet(name = "Servlet")
 public class Servlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String html = "<html><body>Hi. I received param1=" + request.getParameter("param1") + " via POST</body><html>";
-        response.getOutputStream().println(html);
+
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String html = "<html><body>Hi. I received param1=" + request.getParameter("param1") + " via GET</body><html>";
-        response.getOutputStream().println(html);
+        RequestDispatcher dispatcher = null;
+
+        dispatcher = request.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
     }
 }
