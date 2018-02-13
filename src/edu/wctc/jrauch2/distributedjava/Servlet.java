5a43761dd6e327b18f9c4c9da1775609b0621672
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-02-13 00:17:42 -0600

    Initial commit

---
+++ src/edu/wctc/jrauch2/distributedjava/Servlet.java
@@ -0,0 +1,15 @@
+package edu.wctc.jrauch2.distributedjava;
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "Servlet")
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String html = "<html><body>Hi. I received param1=" + request.getParameter("param1") + " via POST</body><html>";
+        response.getOutputStream().println(html);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String html = "<html><body>Hi. I received param1=" + request.getParameter("param1") + " via GET</body><html>";
+        response.getOutputStream().println(html);
+    }
+}
