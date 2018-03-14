f77659a25df184b75e8bbe8e5f093d249373096c
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-03-13 20:40:38 -0500

    new week. Go over cart bean, cart, and cartService

---
+++ NamesApp8/src/main/java/edu/wctc/jrauch2/week8/beans/CartBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wctc.jrauch2.week8.beans;
+
+import edu.wctc.jrauch2.week8.model.Name;
+import edu.wctc.jrauch2.week8.model.ShoppingCart;
+import edu.wctc.jrauch2.week8.model.ShoppingCartService;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author jmbra
+ */
+@Named(value = "cartBean")
+@SessionScoped
+public class CartBean implements Serializable {
+    private final String sessionId;
+    private final ShoppingCart cart;
+    private final ShoppingCartService cartService = new ShoppingCartService();
+    /**
+     * Creates a new instance of CartBean
+     */
+    public CartBean() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        sessionId = facesContext.getExternalContext().getSessionId(true);
+        
+        cart = cartService.getContents(sessionId);
+    }
+    
+    public int getNumberOfItemsInCart() {
+        return cart.getNumberOfItemsInCart();
+    }
+
+    public void addToCart(Name name) {
+        cart.add(name);
+        cartService.update(sessionId, cart);
+    }
+
+}
---
+++ NamesApp8/src/main/java/edu/wctc/jrauch2/week8/model/ShoppingCartService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wctc.jrauch2.week8.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author jmbra
+ */
+public class ShoppingCartService {
+    private static final Map<String, ShoppingCart> contents = new HashMap<>();
+    
+    public ShoppingCart getContents(String sessionId) {
+        ShoppingCart cart = contents.computeIfAbsent(sessionId, (String s) -> new ShoppingCart());
+        
+        return cart;
+    }
+    
+    public void update(String sessionId, ShoppingCart cart) {
+        contents.put(sessionId, cart);
+    }
+    public void delete(String sessionId) {
+        contents.remove(sessionId);
+    }
+
+}
