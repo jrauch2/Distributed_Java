9d1d322e782db70e5b24b50440ff55adcc1f99b5
jrauch2 <35666770+jrauch2@users.noreply.github.com>
2018-03-21 19:15:45 -0500

    Fixed to actually work by copying work done in class.

--- djClassProject/src/main/java/edu/wctc/jrauch2/week6/djclassproject/bean/CartBean.java
+++ djClassProject/src/main/java/edu/wctc/jrauch2/week6/djclassproject/bean/CartBean.java
@@ -1,74 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wctc.jrauch2.week6.djclassproject.bean;
 
+import edu.wctc.jrauch2.week6.djclassproject.model.Cart;
+import edu.wctc.jrauch2.week6.djclassproject.model.CartService;
 import edu.wctc.jrauch2.week6.djclassproject.model.Product;
-import edu.wctc.jrauch2.week6.djclassproject.model.ProductService;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
 import javax.faces.context.FacesContext;
-import javax.faces.event.AjaxBehaviorEvent;
 
-/**
- *
- * @author jmbra
- */
 @Named(value = "cartBean")
 @SessionScoped
 public class CartBean implements Serializable {
-    private final ProductService PRODUCT_SERVICE = new ProductService();
-    private final Map<Product, Integer> CART;
-    private Object productFound;
-    private Product product;
-    private String productID;
 
-    /**
-     * Creates a new instance of CartBean
-     */
-    public CartBean() {
-        CART = new HashMap<>();
-    }
+	private final String sessionId;
+	private final Cart cart;
+	private final CartService cartService = new CartService();
 
-    public Map<Product, Integer> getCART() {
-        return CART;
-    }
-    
-    public void addToCart(AjaxBehaviorEvent event) {
-        try {
-            productID = (String) event.getComponent().getAttributes().get("productID");
-            product = PRODUCT_SERVICE.getProduct(productID);
-            productFound = CART.get(product);
+	public CartBean() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		sessionId = facesContext.getExternalContext().getSessionId(true);
+		cart = cartService.getContents(sessionId);
+	}
 
-            if (productFound == null) {
-                CART.put(product, 1);
-            }
-            else {
-                CART.put(product, CART.get(product) + 1);
-            }
+	public int getNumberOfItemsInCart() {
+		return cart.getNumberOfItemsInCart();
+	}
 
-            FacesContext.getCurrentInstance().getExternalContext().redirect("cart.xhtml");
-        } catch (IOException ex) {
-            Logger.getLogger(ProductBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void updateCart(String productID, int quantity) {
-        product = PRODUCT_SERVICE.getProduct(productID);
-        productFound = CART.get(product);
+	public void addToCart(Product product) {
+		cart.add(product);
+		cartService.update(sessionId, cart);
+	}
         
-        if (productFound != null && quantity == 0) {
-            CART.remove(product);
-        } else if (productFound != null){
-            CART.put(product, quantity);
+        public Cart getCart() {
+            return cart;
         }
-    }    
+
+	
 }
